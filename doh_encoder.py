d6432a98b82206c4f70dafd05d326179f23f3376
shambles1812 <cjvillafuerte02@gmail.com>
2020-03-31 01:50:02 +0800

    encoded

--- doh_encoder.py
+++ doh_encoder.py
@@ -13,7 +13,6 @@ import time
 #load Workbook
 wb = load_workbook('Database3.xlsx')
 alphabet = list(string.ascii_uppercase)
-
 #cell colors
 greenFill = PatternFill(start_color='2ecc71',
                    end_color='2ecc71',
@@ -21,7 +20,6 @@ greenFill = PatternFill(start_color='2ecc71',
 redFill = PatternFill(start_color='e74c3c',
                    end_color='e74c3c',
                    fill_type='solid')
-
 #load resources for a specific worksheet and user
 class get_worksheet_data:
     def __init__(self,worksheet_no,user_number):
@@ -54,7 +52,6 @@ class get_worksheet_data:
                 if i % len(alphabet) == 0:
                     profile_users.append(profile_data)
         self.profile_users = profile_users
-
     def get_all_data(self):
         for data in self.data_list:
             print(str(self.user_number-1) + str(data-1))
@@ -69,17 +66,11 @@ class get_worksheet_data:
     def set_to_not_ok(self,index_no):
             self.sheet[self.profile_users[self.user_number-1][index_no-1]].fill = redFill
            # wb.save('Stylized.xlsx')
-
-
-
-        
 pageData1 = get_worksheet_data(1,1)
 print (get_worksheet_data(1,1).get_specific_data(1))
-
 #main loop 
 # login user -> navigate to encoding page -> encode[ input on fields -> change cell color -> click save] 
 for profile in pageData1.profile_list:
-    
     #login
     browser = webdriver.Chrome('C:\\Users\\Cj\\Desktop\\Temp\\chromedriver')
     browser.get('http://122.54.82.254:65428/')
@@ -91,7 +82,6 @@ for profile in pageData1.profile_list:
     user_field.send_keys(user_input)
     pass_field.send_keys(pass_input)
     login.click()
-
     #navigate to vaccination entry page
     create_new_profile = browser.find_element_by_xpath("//button[@data-toggle='modal']")
     time.sleep(2)
@@ -100,208 +90,181 @@ for profile in pageData1.profile_list:
     time.sleep(2)
     add_vacinee_profile.click()
     time.sleep(2)
-
     # first field paths
     id_field = browser.find_element_by_id('VaccinationCardId')
     #lvl_of_educ_field = browser.find_element_by_id('LevelOfEducation')
-
 # General Information functions
     #checks if the first field has input
     def not_filled(first_field):
         first_field.click()
         current_element = browser.switch_to_active_element()
         if len(current_element.get_attribute("value")) == 0:
-            
             return True
         else:
             return False
-
     def id_input(profile_no):
         if get_worksheet_data(1,profile_no).get_specific_data(1):
             id_field.send_keys(get_worksheet_data(1,profile_no).get_specific_data(1))
             get_worksheet_data(1,profile_no).set_to_ok(1)
         else:
             get_worksheet_data(1,profile_no).set_to_not_ok(1)
-  
-           
+    def press_tab():
+        current_element = browser.switch_to_active_element()
+        current_element.send_keys(Keys.TAB)
+    def press_enter():
+        current_element = browser.switch_to_active_element()
+        current_element.send_keys(Keys.ENTER)
+    def press_right_arrow_key():
+        current_element = browser.switch_to_active_element()
+        current_element.send_keys(Keys.ARROW_RIGHT)
+    def press_left_arrow_key():
+        current_element = browser.switch_to_active_element()
+        current_element.send_keys(Keys.ARROW_LEFT)
+    def click_element():
+        current_element = browser.switch_to_active_element()
+        current_element.click()
 #new method for encoding data on General Info Page
-    numlist =[ 3,4,5,6,7,8,9]
-    numlist2 = [11,12,13]
-    numlist3 = [14,15]
-    numlist4 = [20,21]
-    
     def input_data_on_gen_info(profile):
         #select first element
         id_input(profile)
+        press_tab()
         current_element = browser.switch_to_active_element()
-        current_element.send_keys(Keys.TAB)
-        current_element = browser.switch_to_active_element()
-        #datetime element
         if get_worksheet_data(1,profile).get_specific_data(2):
             current_element.send_keys(get_worksheet_data(1,profile).get_specific_data(2).strftime("%m/%d/%Y"))
             get_worksheet_data(1,profile).set_to_ok(2)
-            current_element.send_keys(Keys.TAB)
         else: 
             get_worksheet_data(1,profile).get_specific_data(2).set_to_not_ok
-            current_element.send_keys(Keys.TAB)
-        for number in numlist:
+        press_tab()
+        for number in list(range(3,10)):
             current_element = browser.switch_to_active_element()
             if get_worksheet_data(1,profile).get_specific_data(number):
                 current_element.send_keys(get_worksheet_data(1,profile).get_specific_data(number))
                 get_worksheet_data(1,profile).set_to_ok(number)
-                current_element.send_keys(Keys.TAB)
             else: 
                 get_worksheet_data(1,profile).set_to_not_ok(number)
-
-                current_element.send_keys(Keys.TAB)
-
+            press_tab()
+            current_element = browser.switch_to_active_element()
         if get_worksheet_data(1,profile).get_specific_data(10):
-                current_element = browser.switch_to_active_element()    
-                current_element.send_keys("0"+ str(get_worksheet_data(1,profile).get_specific_data(10)))
-                get_worksheet_data(1,profile).set_to_ok(10)
-                current_element.send_keys(Keys.TAB)
+            current_element = browser.switch_to_active_element()    
+            current_element.send_keys("0"+ str(get_worksheet_data(1,profile).get_specific_data(10)))
+            get_worksheet_data(1,profile).set_to_ok(10)
         else: 
             current_element = browser.switch_to_active_element()
             get_worksheet_data(1,profile).set_to_not_ok(10)
-            current_element.send_keys(Keys.TAB)
-        for number in numlist2:
+        press_tab()
+        for number in list(range(11,14)):
             current_element = browser.switch_to_active_element()
             if get_worksheet_data(1,profile).get_specific_data(number):
-                current_element.send_keys(Keys.ENTER)
+                press_enter()
                 current_element = browser.switch_to_active_element()
                 current_element.send_keys(get_worksheet_data(1,profile).get_specific_data(number))
-                current_element.send_keys(Keys.ENTER)
+                press_enter()
                 current_element = browser.switch_to_active_element()
                 get_worksheet_data(1,profile).set_to_ok(number)
-                current_element.send_keys(Keys.TAB)
             else:
                 get_worksheet_data(1,profile).set_to_not_ok(number)
-                current_element.send_keys(Keys.TAB)
-        for number in numlist3:
+            press_tab()
+        for number in list(range(14,16)):
             current_element = browser.switch_to_active_element()
             if get_worksheet_data(1,profile).get_specific_data(number):
                 current_element.send_keys(get_worksheet_data(1,profile).get_specific_data(number))
                 get_worksheet_data(1,profile).set_to_ok(number)
-                current_element.send_keys(Keys.TAB)
             else: 
                 get_worksheet_data(1,profile).set_to_not_ok(number)
-                current_element.send_keys(Keys.TAB)         
-        current_element = browser.switch_to_active_element()
+            press_tab()
+            current_element = browser.switch_to_active_element()      
         if get_worksheet_data(1,profile).get_specific_data(16) == "MALE":
-          #  current_element.send_keys(Keys.ARROW_RIGHT)
-           # current_element = browser.switch_to_active_element()
-            current_element.send_keys(Keys.ARROW_LEFT)
+            press_right_arrow_key()
+            current_element = browser.switch_to_active_element()
+            press_left_arrow_key()
             current_element = browser.switch_to_active_element()
             get_worksheet_data(1,profile).set_to_ok(16)
-            current_element.send_keys(Keys.TAB)
         elif get_worksheet_data(1,profile).get_specific_data(16) == "FEMALE":
-            current_element.send_keys(Keys.ARROW_RIGHT)
+            press_right_arrow_key()
             current_element = browser.switch_to_active_element()
-            current_element.send_keys(Keys.ARROW_RIGHT)
+            press_right_arrow_key()
             current_element = browser.switch_to_active_element()
             get_worksheet_data(1,profile).set_to_ok(16)
-            current_element.send_keys(Keys.TAB)
         else:
             get_worksheet_data(1,profile).set_to_not_ok(16)
-            current_element.send_keys(Keys.TAB)
+        press_tab()
+        current_element = browser.switch_to_active_element()
         if get_worksheet_data(1,profile).get_specific_data(17):
-            current_element = browser.switch_to_active_element()
             current_element.send_keys(get_worksheet_data(1,profile).get_specific_data(17))
             get_worksheet_data(1,profile).set_to_ok(17)
-            current_element.send_keys(Keys.TAB)
         else: 
             get_worksheet_data(1,profile).set_to_not_ok(number)
-            current_element.send_keys(Keys.TAB)   
+        press_tab()
+        current_element = browser.switch_to_active_element()
         #set religion
         if get_worksheet_data(1,profile).get_specific_data(18):
             if get_worksheet_data(1,profile).get_specific_data(18) == "ROMAN CATHOLIC":
+                press_right_arrow_key()
                 current_element = browser.switch_to_active_element()
-                current_element.send_keys(Keys.ARROW_RIGHT)
+                press_left_arrow_key()
                 current_element = browser.switch_to_active_element()
-                current_element.send_keys(Keys.ARROW_LEFT)
-                current_element = browser.switch_to_active_element()
-                current_element.send_keys(Keys.TAB)
-                  
                 get_worksheet_data(1,profile).set_to_ok(18)
+                press_tab()
+                current_element = browser.switch_to_active_element()
             elif get_worksheet_data(1,profile).get_specific_data(18) == "CHRISTIAN":
+                press_right_arrow_key()
                 current_element = browser.switch_to_active_element()
-                current_element.send_keys(Keys.ARROW_RIGHT)
-                current_element = browser.switch_to_active_element()
-                current_element.send_keys(Keys.TAB)
-                  
                 get_worksheet_data(1,profile).set_to_ok(18)
+                press_tab()
+                current_element = browser.switch_to_active_element()
             elif get_worksheet_data(1,profile).get_specific_data(18) == "INC" :
-                current_element = browser.switch_to_active_element()
-                current_element.send_keys(Keys.ARROW_RIGHT)
-                current_element = browser.switch_to_active_element()
-                current_element.send_keys(Keys.ARROW_RIGHT)
-                current_element = browser.switch_to_active_element()
-                current_element.send_keys(Keys.TAB)
-            
+                for number in list(range(1,3)):
+                    press_right_arrow_key()
+                    current_element = browser.switch_to_active_element() 
                 get_worksheet_data(1,profile).set_to_ok(18)
+                press_tab()
+                current_element = browser.switch_to_active_element()
             elif get_worksheet_data(1,profile).get_specific_data(18) == "ISLAM" :
-                current_element = browser.switch_to_active_element()
-                current_element.send_keys(Keys.ARROW_RIGHT)
-                current_element = browser.switch_to_active_element()
-                current_element.send_keys(Keys.ARROW_RIGHT)
-                current_element = browser.switch_to_active_element()
-                current_element.send_keys(Keys.ARROW_RIGHT)
-                current_element = browser.switch_to_active_element()
-                current_element.send_keys(Keys.TAB)
-                
+                for number in list(range(1,4)):
+                    press_right_arrow_key()
+                    current_element = browser.switch_to_active_element() 
                 get_worksheet_data(1,profile).set_to_ok(18)
+                press_tab()
+                current_element = browser.switch_to_active_element()
             elif get_worksheet_data(1,profile).get_specific_data(18) == "JEHOVAH" :
-                current_element = browser.switch_to_active_element()
-                current_element.send_keys(Keys.ARROW_RIGHT)
-                current_element = browser.switch_to_active_element()
-                current_element.send_keys(Keys.ARROW_RIGHT)
-                current_element = browser.switch_to_active_element()
-                current_element.send_keys(Keys.ARROW_RIGHT)
-                current_element = browser.switch_to_active_element()
-                current_element.send_keys(Keys.ARROW_RIGHT)
-                current_element = browser.switch_to_active_element()
-                current_element.send_keys(Keys.TAB) 
-                
+                for number in list(range(1,5)):
+                    press_right_arrow_key()
+                    current_element = browser.switch_to_active_element() 
                 get_worksheet_data(1,profile).set_to_ok(18)
+                press_tab()
+                current_element = browser.switch_to_active_element()
             else: 
-                
-                for number in [1,2,3,4,5]:
-                    current_element = browser.switch_to_active_element()
-                    current_element.send_keys(Keys.ARROW_RIGHT) 
-                current_element.send_keys(Keys.TAB)
+                for number in list(range(1,6)):
+                    press_right_arrow_key()
+                    current_element = browser.switch_to_active_element() 
+                press_tab()
                 current_element = browser.switch_to_active_element()
                 current_element.send_keys(get_worksheet_data(1,1).get_specific_data(18))
                 get_worksheet_data(1,profile).set_to_ok(18)
-                current_element.send_keys(Keys.TAB)
         else:
             get_worksheet_data(1,profile).set_to_not_ok(18)
-        
+        press_tab()
         if get_worksheet_data(1,profile).get_specific_data(19):
             current_element = browser.switch_to_active_element()
             current_element.send_keys(get_worksheet_data(1,profile).get_specific_data(19).strftime("%m/%d/%Y"))
             get_worksheet_data(1,profile).set_to_ok(19)
-            current_element.send_keys(Keys.TAB)
-        else: 
-            get_worksheet_data(1,profile).get_specific_data(19).set_to_not_ok
-           #if forgotBirthdate_selection:
-                #forgotBirthdate_selection.click()
-                #current_element = browser.switch_to_active_element()
-                #current_element.send_keys(Keys.TAB)
-           # else:
-            get_worksheet_data(1,profile).get_specific_data(19).set_to_not_ok
+            press_tab()
             current_element = browser.switch_to_active_element()
-            current_element.send_keys(Keys.TAB)
-            
-        for number in numlist4:
+        else: 
+            get_worksheet_data(1,profile).set_to_not_ok(19)
+            current_element = browser.switch_to_active_element()
+            press_tab()
+            current_element = browser.switch_to_active_element()
+            click_element()
+        press_tab()
+        for number in list(range(20,22)):
             current_element = browser.switch_to_active_element()
             if get_worksheet_data(1,profile).get_specific_data(number):
                 current_element.send_keys(get_worksheet_data(1,profile).get_specific_data(number))
                 get_worksheet_data(1,profile).set_to_ok(number)
-                current_element.send_keys(Keys.TAB)
             else: 
                 get_worksheet_data(1,profile).set_to_not_ok(number)
-                current_element.send_keys(Keys.TAB)  
-
+            press_tab() 
         wb.save('Stylized3.xlsx')
         input('wait')
     functions_list = ['input_data_on_gen_info(profile)','id_input']
